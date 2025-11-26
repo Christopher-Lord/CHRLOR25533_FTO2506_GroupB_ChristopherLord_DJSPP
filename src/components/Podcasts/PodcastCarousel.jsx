b0f8c4d6036eb1075367a0e0a7a2305827609518
Christopher-Lord <lordc42@gmail.com>
2025-11-26 11:35:29 +0200

    Add PodcastCarousel component that displays a random selection of podcasts

---
+++ src/components/Podcasts/PodcastCarousel.jsx
@@ -0,0 +1,62 @@
+import { usePodcasts } from "../../context/PodcastContext";
+import { Link } from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
+import "./PodcastCard.css";
+import "./PodcastCarousel.css";
+
+export default function PodcastCarousel({ numberOfItems }) {
+  const { podcasts, isLoading, error } = usePodcasts();
+
+  const [shuffled, setShuffled] = useState([]);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (podcasts.length === 0) return;
+
+    const shuffledPodcasts = [...podcasts].sort(() => Math.random() - 0.5);
+    setShuffled(shuffledPodcasts);
+  }, [podcasts]);
+
+  if (isLoading) return;
+  if (error) return;
+
+  const randomSelection = shuffled.slice(0, numberOfItems);
+
+  return (
+    <>
+      <div className="carousel-container">
+        <h2 className="recommended">Recommended</h2>
+        <button className="carousel-arrow left">
+          &#10094;
+        </button>
+        <div className="carousel-content" ref={containerRef}>
+          {randomSelection.map((pod) => (
+            <Link to={`/show/${pod.id}`} className="link" key={pod.id}>
+              <div className="carousel-item">
+                <img
+                  src={pod.image}
+                  alt={pod.title}
+                  className="preview-cover-img"
+                />
+
+                <h2>{pod.title}</h2>
+
+                <p className="podcast-seasons">{pod.seasons} seasons</p>
+
+                <ul className="podcast-genres">
+                  {/* Loop through the array of genres and render each one as a list item */}
+                  {pod.genres.map((genre) => (
+                    <li key={genre}>{genre}</li>
+                  ))}
+                </ul>
+              </div>
+            </Link>
+          ))}
+        </div>
+        <button className="carousel-arrow right">
+          &#10095;
+        </button>
+      </div>
+    </>
+  );
+}
