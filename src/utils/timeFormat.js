f3ef9544dd7071205cb4ee32172915ccdb5c1c2c
Christopher-Lord <lordc42@gmail.com>
2025-11-26 11:24:24 +0200

    Add function to format time correctly

---
+++ src/utils/timeFormat.js
@@ -0,0 +1,8 @@
+export function timeFormat(totalSeconds) {
+  if (!totalSeconds) return "0:00";
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = Math.floor(totalSeconds % 60)
+    .toString()
+    .padStart(2, "0");
+  return `${minutes}:${seconds}`;
+}
