d27ad0e1b0139a4746a311290efa2320247361aa
Christopher-Lord <lordc42@gmail.com>
2025-11-26 11:23:12 +0200

    Add time tracking and leave alert for when audio is playing

--- src/hooks/useAudioPlayer.jsx
+++ src/hooks/useAudioPlayer.jsx
@@ -1,10 +1,13 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export function useAudioPlayer() {
   const audio = useRef(new Audio()).current;
 
   const [track, setTrack] = useState(null);
   const [playing, setPlaying] = useState(false);
+
+  const [progress, setProgress] = useState(0);
+  const [duration, setDuration] = useState(0);
 
   const playEpisode = useCallback(
     (episode) => {
@@ -31,9 +34,40 @@ export function useAudioPlayer() {
     audio.currentTime = value;
   }
 
+  useEffect(() => {
+    function update() {
+      setProgress(audio.currentTime);
+      setDuration(audio.duration);
+    }
+
+    audio.ontimeupdate = update;
+    audio.onloadedmetadata = update;
+
+    return () => {
+      audio.ontimeupdate = null;
+      audio.onloadedmetadata = null;
+    };
+  }, [audio]);
+
+  useEffect(() => {
+    function leaveAlert(event) {
+      if (!playing) return;
+      event.preventDefault();
+      event.returnValue = "";
+    }
+
+    window.onbeforeunload = leaveAlert;
+
+    return () => {
+      window.onbeforeunload = null;
+    };
+  }, [playing]);
+
   return {
     track,
     playing,
+    progress,
+    duration,
     playEpisode,
     play,
     pause,
