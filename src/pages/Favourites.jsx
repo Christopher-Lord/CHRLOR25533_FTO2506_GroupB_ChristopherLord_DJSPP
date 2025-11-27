4e39be8402e6b7c935d98e6002556c7a5d02ced0
Christopher-Lord <lordc42@gmail.com>
2025-11-27 12:11:23 +0200

    Update component UI/ Add play btn functionality

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,8 +1,13 @@
-import { useMemo } from "react";
+import { useMemo, useState, useEffect } from "react";
 import { useFavourites } from "./../context/FavouritesContext";
+import { useAudioPlayerContext } from "../context/AudioPlayerContext";
+import { fetchSinglePodcast } from "../api/fetchData";
+import { useParams } from "react-router-dom";
 
 export default function Favourites() {
   const { favourites, removeFavourite } = useFavourites();
+
+  const { playEpisode } = useAudioPlayerContext();
 
   const groupedByShow = useMemo(() => {
     const groups = {};
@@ -25,26 +30,54 @@ export default function Favourites() {
       ) : (
         Object.entries(groupedByShow).map(([showTitle, episodes]) => (
           <div key={showTitle} className="show-group">
-            <h2>{showTitle}</h2>
+            <div className="fav-header">
+              <img src={episodes[0].showImage} alt={showTitle} />
+              <h2>{showTitle}</h2>
+            </div>
             <div className="episode-list">
               {episodes.map((ep) => (
                 <div
                   key={`${ep.showId}-${ep.season}-${ep.episodeNumber}`}
                   className="episode-card"
                 >
+                  {/* All episodes re-use season cover image */}
                   <img
+                    className="episode-img"
                     src={ep.seasonImage}
                     alt={ep.title}
-                    className="episode-img"
                   />
-                  <div className="episode-info">
+                  <div className="episode-content">
                     <h4>Season {ep.season}</h4>
-                    <p>
+                    <h5>
                       Episode {ep.episodeNumber}: {ep.title}
-                    </p>
+                    </h5>
                     <p>Added {new Date(ep.addedAt).toLocaleString()}</p>
                   </div>
-                  <button onClick={() => removeFavourite(ep)}>Remove</button>
+                  <div className="ep-btns">
+                    <div
+                      className="ep-play-btn"
+                      onClick={() =>
+                        playEpisode({
+                          title: ep.title,
+                          episode: ep.episode,
+                          file: ep.file,
+                          podcastTitle: ep.showTitle,
+                          season: ep.season,
+                          seasonImage: ep.seasonImage,
+                        })
+                      }
+                    >
+                      <span>&#9654;</span>
+                    </div>
+                    <div className="remove-fav-btn">
+                      <span onClick={() => removeFavourite(ep)}>
+                        <img
+                          className="remove-fav-img"
+                          src="./src/assets/remove-fav-btn.png"
+                        />
+                      </span>
+                    </div>
+                  </div>
                 </div>
               ))}
             </div>
