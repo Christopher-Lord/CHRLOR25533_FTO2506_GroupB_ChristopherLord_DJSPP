9bd7af370b35647dab2590285e755a20a51c0fe5
Christopher-Lord <lordc42@gmail.com>
2025-11-27 08:30:21 +0200

    Add UI elements for Home and Favourites pages

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,13 +1,25 @@
+import { NavLink } from "react-router-dom";
+
 /**
  * Header component
- * 
+ *
  * @returns {JSX.Element} Header UI
  */
 export default function Header() {
   return (
     <header className="podcast-header">
-      <img src="/src/assets/podcast-logo-light.png" alt="Podcast Logo" />
-      <h1>PodcastApp</h1>
+      <div className="podcast-logo">
+        <img src="/src/assets/podcast-logo-light.png" alt="Podcast Logo" />
+        <h1>PodcastApp</h1>
+      </div>
+      <div className="nav-links">
+        <NavLink to={"/"} className="link">
+          <h3>Home</h3>
+        </NavLink>
+        <NavLink to={"/favourites"} className="link">
+          <h3>Favourites</h3>
+        </NavLink>
+      </div>
     </header>
   );
 }
