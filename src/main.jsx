edfd3db0c44876d51367718dfee90093b744705a
Christopher-Lord <lordc42@gmail.com>
2025-11-27 08:44:05 +0200

    Add FavouritesProvider to root render

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AudioPlayerProvider } from "./context/AudioPlayerContext.jsx";
+import { FavouritesProvider } from "./context/FavouritesContext.jsx";
 
 /**
  * Mounts the "App" React component to the DOM element with the ID "root"
@@ -9,7 +10,9 @@ import { AudioPlayerProvider } from "./context/AudioPlayerContext.jsx";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AudioPlayerProvider>
-      <App />
+      <FavouritesProvider>
+        <App />
+      </FavouritesProvider>
     </AudioPlayerProvider>
   </BrowserRouter>,
 );
