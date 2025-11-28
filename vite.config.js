c94c3120e399509e519d2add2baecae50aabe0db
Christopher-Lord <lordc42@gmail.com>
2025-11-28 14:46:52 +0200

    Add base to defineConfig

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base:
+    process.env.VITE_BASE_PATH ||
+    "/CHRLOR25533_FTO2506_GroupB_ChristopherLord_DJSPP",
+});
